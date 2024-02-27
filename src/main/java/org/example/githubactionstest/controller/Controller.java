fc69b244dae4a48d15e06df617f8bed4e84e967b
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-02-27 10:49:44 +0100

    added controller

---
+++ src/main/java/org/example/githubactionstest/controller/Controller.java
@@ -0,0 +1,11 @@
+package org.example.githubactionstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("")
+    public String getWelcome() {
+        return "welcome";
+    }
+}
--- src/test/java/org/example/githubactionstest/GitHubActionsTestApplicationTests.java
+++ src/test/java/org/example/githubactionstest/GitHubActionsTestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitHubActionsTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
