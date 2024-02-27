0117113bd39995e62dd4caad34afa1108652b8af
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-02-27 10:20:02 +0100

    Initial commit

---
+++ src/test/java/org/example/githubactionstest/GitHubActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
